aa48eb27431a37c3a3e1b4b0fea077e0988785f0
Loi <129076315+JBL-987@users.noreply.github.com>
2023-09-03 22:39:46 +0700

    Update Question.py

--- Prank-Question/Question.py
+++ Prank-Question/Question.py
@@ -3,13 +3,9 @@ from tkinter import messagebox
 import pywhatkit
 import random
 
-
-def on_closing():
-    if messagebox.askokcancel("Quit", "Lol gay"):
-        root.destroy()
-
-
 def yes():
+    if messagebox.askokcancel("Quit", "Lol gay"):
+    root.destroy()
     url = "https://youtube.be/nZJJ4m18SPg?si=IETjUbzmNKvF5luO"
     pywhatkit.playonyt(url)
 
@@ -23,6 +19,7 @@ root.title("Question")
 root.geometry("800x800")
 root.resizable(width=False, height=False)
 root['bg'] = 'white'
+root.overrideredirect(True)
 
 label = Label(root, text='Are you Gay?',
               font='Arial 20 bold', bg='white').pack()
@@ -36,5 +33,4 @@ btnNo.bind('<Enter>', motionMouse)
 btnYes = Button(root, text='Yes', font='Arial 20 bold',
                 command=yes).place(x=350, y=400)
 
-root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
